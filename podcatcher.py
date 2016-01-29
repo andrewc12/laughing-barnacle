7f5f159483dca2721b5e649df15d82cf01ea32a8
Andrew Innes <andrew.c12@gmail.com>
2016-01-29 02:00:57 -0500

    load configuration from file

--- podcatcher.py
+++ podcatcher.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 import argparse
+import yaml
 
 #set_defaults
+def set_defaults():
+	config = 'config.yml'
+	return
+set_defaults()
 
 parser = argparse.ArgumentParser(description='TODO insert description')
 
 parser.add_argument('--config', '-c')
-
 parser.add_argument('--verbose',
 	action='store_true',
 	help='verbose flag' )
@@ -17,6 +21,9 @@ args = parser.parse_args()
 if args.config:
 	config = args.config
 #load_config
+with open(config, 'r') as ymlfile:
+	cfg = yaml.load(ymlfile)
+
 
 
 if args.verbose:
